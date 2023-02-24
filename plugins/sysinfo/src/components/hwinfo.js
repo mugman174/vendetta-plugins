35c7d67070388f4edffd9d916f9cd1af54a9bc0f
mugman <53713683+mugman174@users.noreply.github.com>
2023-02-24 13:23:05 -0800

    [sysinfo > components > hwinfo] capitalization of network states

--- plugins/sysinfo/src/components/hwinfo.js
+++ plugins/sysinfo/src/components/hwinfo.js
@@ -5,9 +5,19 @@ function hwinfo() {
   const cpuPerc = cpuPercentage.toFixed(2) + "%";
   const memUsage = parseFloat((memory / 1000).toPrecision(3)) + " MB";
   const { type, details } = findByProps("useNetInfo").fetch()._55;
-  let netInfo = type;
+  let netInfo = "";
   if (type == "cellular") {
-    netInfo += ` ${details.cellularGeneration}`;
+    netInfo = `Cellular ${details.cellularGeneration.toUpperCase()}`;
+  } else {
+    const networkMap = {
+      wifi: "WiFi",
+      ethernet: "Ethernet",
+      other: "Unknown",
+      bluetooth: "Bluetooth",
+      wimax: "WiMAX",
+      vpn: "VPN",
+    };
+    netInfo = networkMap[type] ?? type;
   }
   return { cpuCoreCount, cpuPerc, memUsage, netInfo };
 }
