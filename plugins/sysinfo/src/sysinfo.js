90eeb57faa71de8d25fdca44fa2ab731900f2b01
mugman <53713683+mugman174@users.noreply.github.com>
2023-03-04 10:32:33 -0800

    [sysinfo > sysinfo.js] show alert when error

--- plugins/sysinfo/src/sysinfo.js
+++ plugins/sysinfo/src/sysinfo.js
@@ -1,5 +1,6 @@
 import { React, ReactNative } from "@vendetta/metro/common";
 import { version } from "@vendetta";
+import { showConfirmationAlert } from "@vendetta/ui/alerts";
 import {
   deviceinfo,
   hwinfo,
@@ -9,62 +10,68 @@ import {
 } from "./components";
 
 function genDebug() {
-  const { cpuCoreCount, cpuPerc, memUsage, netInfo } = hwinfo();
+  try {
+    const { cpuCoreCount, cpuPerc, memUsage, netInfo } = hwinfo();
 
-  const { Version, Build, ReleaseChannel } = discordinfo();
+    const { Version, Build, ReleaseChannel } = discordinfo();
 
-  const {
-    device,
-    deviceManufacturer,
-    deviceBrand,
-    deviceModel,
-    systemVersion,
-    width,
-    height,
-  } = deviceinfo();
-  const { osName, osVersion } = softinfo();
+    const {
+      device,
+      deviceManufacturer,
+      deviceBrand,
+      deviceModel,
+      systemVersion,
+      width,
+      height,
+    } = deviceinfo();
+    const { osName, osVersion } = softinfo();
 
-  const { ReactVersion, RNVersion, HermesBytecode, HermesRelease } =
-    reactinfo();
+    const { ReactVersion, RNVersion, HermesBytecode, HermesRelease } =
+      reactinfo();
 
-  let output = {
-    Device: {
-      Device: device,
-      Model: deviceModel,
-      Manufacturer: deviceManufacturer,
-      Brand: deviceBrand,
-      Display: width + "x" + height,
-    },
-    Hardware: {
-      "CPU Cores": cpuCoreCount,
-      Network: netInfo,
-    },
-    Software: {
-      OS: osName,
-      Version: osVersion,
-    },
-    Discord: {
-      Branch: ReleaseChannel,
-      Version: Version,
-      Build: Build,
-      Vendetta: version,
-      "CPU Usage": cpuPerc,
-      "Memory Usage": memUsage,
-    },
-    React: {
-      Version: ReactVersion,
-      "Hermes Bytecode": HermesBytecode,
-      Hermes: HermesRelease,
-      Native: RNVersion,
-    },
-  };
-  if (window.enmity) {
-    output.Discord.Enmity = window.enmity.version;
+    let output = {
+      Device: {
+        Device: device,
+        Model: deviceModel,
+        Manufacturer: deviceManufacturer,
+        Brand: deviceBrand,
+        Display: width + "x" + height,
+      },
+      Hardware: {
+        "CPU Cores": cpuCoreCount,
+        Network: netInfo,
+      },
+      Software: {
+        OS: osName,
+        Version: osVersion,
+      },
+      Discord: {
+        Branch: ReleaseChannel,
+        Version: Version,
+        Build: Build,
+        Vendetta: version,
+        "CPU Usage": cpuPerc,
+        "Memory Usage": memUsage,
+      },
+      React: {
+        Version: ReactVersion,
+        "Hermes Bytecode": HermesBytecode,
+        Hermes: HermesRelease,
+        Native: RNVersion,
+      },
+    };
+    if (window.enmity) {
+      output.Discord.Enmity = window.enmity.version;
+    }
+    if (window.aliucord) {
+      output.Discord.Aliucord = window.aliucord.version;
+    }
+    return output;
+  } catch (e) {
+    showConfirmationAlert({ title: "Error | Send to mugman#0043", content: e });
+    console.trace(e);
+    throw e;
   }
-  if (window.aliucord) {
-    output.Discord.Aliucord = window.aliucord.version;
-  }
-  return output;
 }
 
 export default genDebug;
