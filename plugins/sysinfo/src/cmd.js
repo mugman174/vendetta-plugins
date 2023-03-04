a0bebc0ed156b7846e74eaec5f5a7ad7ff57bd4b
mugman <53713683+mugman174@users.noreply.github.com>
2023-03-04 10:26:12 -0800

    [sysinfo > cmd.js] format code

--- plugins/sysinfo/src/cmd.js
+++ plugins/sysinfo/src/cmd.js
@@ -1,15 +1,15 @@
 import genDebug from "./sysinfo";
 
 function sysinfocmd(args, ctx) {
-    let output = ["__System Information__\n"];
-    const data = genDebug();
-    Object.keys(data).forEach((option) => {
-      output.push(`[**${option}**]`);
-      Object.keys(data[option]).forEach((subOption) => {
-        output.push(`> ${subOption}: ${data[option][subOption]}`);
-      });
+  let output = ["__System Information__\n"];
+  const data = genDebug();
+  Object.keys(data).forEach((option) => {
+    output.push(`[**${option}**]`);
+    Object.keys(data[option]).forEach((subOption) => {
+      output.push(`> ${subOption}: ${data[option][subOption]}`);
     });
-    return { content: output.join("\n") };
+  });
+  return { content: output.join("\n") };
 }
 
 export default sysinfocmd;
