4b43750182fac6cc6231d439a88e80d98e658f0e
Meqa <106783764+Meqativ@users.noreply.github.com>
2023-04-28 22:01:39 +0300

    more

--- plugins/urban/src/urban.js
+++ plugins/urban/src/urban.js
@@ -1,31 +1,31 @@
 import { findByProps } from "@vendetta/metro";
-
+const { sendBotMessage } = findByProps("sendBotMessage");
+const FORUM_POST_URL = "https://discord.com/channels/1015931589865246730/1092643779523121262";
 export default async function urbanDef(args, ctx) {
 	try {
-		const { sendBotMessage } = findByProps("sendBotMessage");
 		const options = new Map(args.map((option) => [option.name, option]));
 
 		const word = options.get("word").value.trim();
 		const query = encodeURIComponent(word);
 		const url = `https://api.urbandictionary.com/v0/define?term=${query}`;
-		const data = await fetch(url).then(res=>res.json());
+		const response = await fetch(url),
+			data = await response.json();
 
 		const inline_links = options.get("inline_links")?.value || false;
 		const defObj = data.list?.[0];
 		const definition = defObj?.definition;
 		if (!definition) {
-			return sendBotMessage(ctx.channel.id, `No definition found for \`${word.replaceAll("`", "`󠄴")}\`.`);
+			return sendBotMessage(ctx.channel.id, `No definition found for \`${word.replaceAll("`", "`󠄴")}\`${response.status !== 200 ? ` (${response.status})` : ""}`);
 		}
 		const permalink = defObj.permalink;
-		let output = `__Definition for \`${defObj.word}\`__`;
+		let output = `__Definition for **\`${defObj.word}\`**__`;
 		
 		if (!inline_links) {
 			output += `\n${quote(definition)}\n\n`+
-								`Source: <${permalink}>`;
+			`Source: <${permalink}>`;
 		} else {
 			output += ` ([source](${permalink} "Link to the place where the definition was found"))\n`+
-				`${quote(replaceHighlighted(definition))}`
-			
+			`${quote(replaceHighlighted(definition))}`;
 		}
 
 
@@ -38,13 +38,14 @@ export default async function urbanDef(args, ctx) {
 		console.error(error);
 		sendBotMessage(
 			ctx.channel.id,
-			"An error occurred while fetching the definition.\n" +
-				`(${error.message})`
+			`\`\`\`js\n${error.stack}\`\`\``+
+			"An error occurred while processing the command\n" +
+			"Send the error in ${FORUM_POST_URL}, to get this issue solved (hopefully)"
 		);
 	}
 }
 function quote(text) {
-	return `> ${text.replaceAll("\n", "\n> ")}`
+	return `> ${text.replaceAll("\n", "\n> ")}`;
 }
 function replaceHighlighted(text) {
 	return text.replace(
